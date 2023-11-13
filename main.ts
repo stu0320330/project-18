054aaf15c69f78093e1906f8d01c895c43ff17c3
Savinu Nugith Dulwan Wimalasuriya <150595620+stu0320330@users.noreply.github.com>
2023-11-13 17:07:05 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.showIcon(IconNames.Heart)
